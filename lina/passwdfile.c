6df818281b4cd939101dd741fe8c17dc2fb39308
Kulina <kulinasys@gmail.com>
2023-03-01 12:15:02 +0530

    CheckLogin

--- lina/passwdfile.c
+++ lina/passwdfile.c
@@ -463,29 +463,6 @@ int ProcessPasswd(char *Passwd) {
   while( (i> 0)&&(Passwd[i]<=' ')) {Passwd[i]='\0';i--;}
   return 1;
 }
-int CheckLogin_o(LINACONFIG *lc,int Index,char *password) {
-    int ret=0;
-    char *p, *correct, *supplied, *salt;
-    if (Index<=0 ) return 0;
-    ProcessPasswd(password);
-    correct = GetUserPw(lc,Index);
-    salt = strdup(correct);
-    if (salt == NULL) return 2;
-    p = strchr(salt + 1, '$');
-    if (p == NULL) ret= 2;
-    else {
-     p = strchr(p + 1, '$');
-     if (p == NULL) ret= 3;
-     else {
-      p[0] = 0;
-      supplied = crypt(password, salt);
-      if (supplied == NULL) ret= 4;
-      else ret= !strcmp(supplied, correct);
-     }
-    }
-    free(salt);
-    return ret;
-}
 int CheckLogin(LINACONFIG *lc,int Index,char *password) {
     int ret=0;
     char *p, *correct, *supplied, *salt;
@@ -498,7 +475,6 @@ int CheckLogin(LINACONFIG *lc,int Index,char *password) {
     if (supplied == NULL) ret= 4; 
     else{   
        ret= !strcmp(supplied, correct);
-       free(supplied);
     }
     free(salt);
     return ret;
